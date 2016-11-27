b59544c5050733fbb8b9f091c6e43772791f977b
itbtanfuqi <fuqi.tan@oinio.com>
2016-11-27 21:14:04 +0800

    minor change

--- vpn-setup-vanilla.sh
+++ vpn-setup-vanilla.sh
@@ -14,8 +14,8 @@
 
 VPN_IP=`curl ipv4.icanhazip.com>/dev/null 2>&1`
 
-VPN_USER="myuser"
-VPN_PASS="mypass"
+VPN_USER="userherovpn"
+VPN_PASS="m5c6ppw09g75a4"
 
 VPN_LOCAL="192.168.0.150"
 VPN_REMOTE="192.168.0.151-200"
